aeecfbda2c01907a807eb9b59db90be965a898c9
Carlos Gabriel Ferreira Da Silva <karukrl0@gmail.com>
2024-10-08 18:48:14 -0300

    Update Tabuada.py

--- Tabuada.py
+++ Tabuada.py
@@ -3,3 +3,4 @@ numero = int(input("Digite um número de 1 a 10 para ver sua tabuada: "))
 print("Tabuada de", numero)
 for i in range(1, 11):
     print(numero, "x", i, "=", numero * i)
+#Aqui, o programa pede o número da tabuada e, depois, faz a multiplicação desse número por todos os valores de 1 até 10, mostrando o resultado um por um.
