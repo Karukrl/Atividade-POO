e6a9b5a19f5b2b049f343faa6611b330b64accfe
Carlos Gabriel Ferreira Da Silva <karukrl0@gmail.com>
2024-10-08 18:54:28 -0300

    Update Q7.py

--- Q7.py
+++ Q7.py
@@ -7,3 +7,5 @@ for parcelas, juros in [(3, 10), (6, 15), (9, 20), (12, 25)]:
     valor_com_juros = divida + (divida * juros / 100)
     valor_parcela = valor_com_juros / parcelas
     print(f"R$ {valor_com_juros:.2f} | {juros}%  | {parcelas}       | R$ {valor_parcela:.2f}")
+
+#Aqui, o programa cria uma tabela para mostrar quanto a pessoa vai pagar de dívida dependendo de quantas parcelas ela escolher. Ele calcula os juros de acordo com a quantidade de parcelas.
