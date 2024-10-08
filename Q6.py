de42bf6a8aade3ce02f45eb84f42b1677986b3b2
Carlos Gabriel Ferreira Da Silva <karukrl0@gmail.com>
2024-10-08 18:53:30 -0300

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -6,3 +6,4 @@ for ano in range(1996, 2026):
     aumento *= 2  # O aumento dobra a cada ano
 
 print("O salário em 2025 será de R$", salario)
+#Esse código calcula o salário de um funcionário desde 1995, onde ele ganhava R$ 1.000,00. A cada ano, o salário aumenta. No primeiro ano, o aumento é de 1,5%, e depois o aumento dobra a cada ano.
