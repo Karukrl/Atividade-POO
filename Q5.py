06e49bfcfa81dea72c274ae079fb42ddc6b05543
Carlos Gabriel Ferreira Da Silva <karukrl0@gmail.com>
2024-10-08 18:52:37 -0300

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -1,3 +1,4 @@
 print("Lojas Quase Dois - Tabela de preços")
 for quantidade in range(1, 51):
     print(quantidade, "- R$", quantidade * 1.99)
+#Aqui o programa faz uma tabela de preços. Para cada item que a pessoa compra, ele mostra o preço total, multiplicando o número de itens pelo valor de R$ 1,99
